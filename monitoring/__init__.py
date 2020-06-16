a432afa59d50a8b83a5b204015e83ced26656cf7
shengyu <shengyuemail@126.com>
2020-06-16 17:07:08 +0800

    point added

--- monitoring/__init__.py
+++ monitoring/__init__.py
@@ -1,35 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for,render_template
 import pymysql.cursors
 from monitoring.map import bp
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:3306/{DB_NAME}'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pmysql://root:sy123456@localhost:3306/mydb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.register_blueprint(bp)
 
-
 @app.route('/')
-def hello():
+def index():
+    return render_template('base.html')
 
-    connection = pymysql.connect(host='localhost',
-                            user='root',
-                             password='sy123456',
-                             db='mydb',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-    try:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM kudata"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result[0])
-
-    finally:
-        connection.close()
-
-    return "hello world"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- monitoring/map.py
+++ monitoring/map.py
@@ -1,7 +1,33 @@
 from flask import Blueprint, render_template
+import pymysql.cursors
 
 bp = Blueprint('/map', __name__, url_prefix='/map')
 
-@bp.route('/',methods=('GET', 'POST'))
+def dataConnect(query):
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='sy123456',
+                                 db='mydb',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            #sql = "SELECT * FROM kudata"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            print(result)
+            return result
+    finally:
+        connection.close()
+
+
+@bp.route('/', methods=('GET', 'POST'))
 def map():
-    return render_template('base.html')
+    query = "select * from kudata"
+    data = dataConnect(query)
+    longtitude = data[0]['longtitude']
+    latitude = data[0]['latitude']
+    return render_template('base.html', longtitude=longtitude, latitude=latitude)
+
+
