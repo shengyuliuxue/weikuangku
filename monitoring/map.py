25068c2b213bcc0b527b66a9f8573598312eec3b
shengyugithub <594549026@qq.com>
2020-07-26 22:26:51 +0800

    echarts added

--- monitoring/map.py
+++ monitoring/map.py
@@ -2,6 +2,17 @@
 from flask import Blueprint, render_template, request
 import pymysql.cursors
 import json
+import datetime
+
+
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime("%Y-%m-%d")
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 bp = Blueprint('/map', __name__, url_prefix='/map')
 
@@ -39,9 +50,10 @@ def ajaxtest():
     query = 'select * from diandata where kuId=1'
     #print (query)
     result = dataConnect(query)
-    print(result)
+    query2 = 'select * from waterdata where CdianId=11'
+    result2 = dataConnect(query2)
 
-    return render_template('kudata.html', kudata=result)
+    return render_template('kudata.html', kudata=result, waterdata=result2)
 
 @bp.route('/status', methods=('GET', 'POST'))
 def status():
@@ -56,3 +68,10 @@ def kuPoints():
     resultlist = dataConnect(query)
     jsondata = json.dumps(resultlist, ensure_ascii=False)
     return jsondata
+
+@bp.route('/waterdata', methods=('GET', 'POST'))
+def waterdata():
+    query = 'select * from waterdata'
+    resultlist = dataConnect(query)
+    waterjson = json.dumps(resultlist, ensure_ascii=False, cls=DateEncoder)
+    return  waterjson
